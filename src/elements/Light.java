05d517e14f948417c61b6fb7706a0760dcb9e9bd
YEHUDA\yehuda <ylev1988@gmail.com>
2017-05-17 20:48:59 +0300

    add new element- Light Light class

--- src/elements/Light.java
+++ src/elements/Light.java
@@ -7,18 +7,22 @@ import java.awt.Color;
 public class Light {
 	
 	protected Color _color;
+	
+	//default cons the most light-white
 	public Light(){
-		_color=new Color(0,0,0);
+		
+		_color=new Color(255, 255, 255);
 	}
+	
+	//copy cons
 	public Light (Color color)
 	{
 		_color=new Color(color.getRGB());
 	}
 	
+	//operations
 	public Color getIntensity()
 	{
-		
 		return null;
 	}
-
 }
