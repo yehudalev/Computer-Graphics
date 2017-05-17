7de46ee94cd379b7f2205d8cd156f6dda4a2759e
YEHUDA\yehuda <ylev1988@gmail.com>
2017-05-17 21:41:34 +0300

    render and image writer tests

---
+++ src/unitTest/RenderTest.java
@@ -0,0 +1,58 @@
+package unitTest;
+
+import geometries.*;
+import primitives.*;
+
+import renderer.*;
+import scene.*;
+import elements.*;
+import java.awt.Color;
+
+import org.junit.Test;
+
+public class RenderTest 
+{
+	 
+    @Test
+    public void basicRendering(){
+         
+    	Scene scene = new Scene(new AmbientLight(255,255,255), 
+				    			new Color(0.3f,0.5f,0.74f), 
+				    			new Camera(), 40);
+         
+        scene.addGeometry(new Sphere(50, new Point3D(0.0, 0.0, -50)));
+         
+        Triangle triangle = new Triangle(new Point3D( 100, 0, -49),
+                                         new Point3D(  0, 100, -49),
+                                         new Point3D( 100, 100, -49));
+         
+        Triangle triangle2 = new Triangle(new Point3D( 100, 0, -49),
+                                          new Point3D(  0, -100, -49),
+                                          new Point3D( 100,-100, -49));
+         
+        Triangle triangle3 = new Triangle(new Point3D(-100, 0, -49),
+                                          new Point3D(  0, 100, -49),
+                                          new Point3D(-100, 100, -49));
+         
+        Triangle triangle4 = new Triangle(new Point3D(-100, 0, -49),
+                                          new Point3D(  0,  -100, -49),
+                                          new Point3D(-100, -100, -49));
+         
+        scene.addGeometry(triangle);
+        scene.addGeometry(triangle2);
+        scene.addGeometry(triangle3);
+        scene.addGeometry(triangle4);
+         
+        ImageWriter imageWriter = new ImageWriter("Render test", 500, 500, 500, 500);
+         
+        Render render = new Render(imageWriter, scene);
+         
+        render.renderImage();
+       // render.printGrid(50);
+        render.writeToImage();
+         
+    }
+     
+}
+     
+
