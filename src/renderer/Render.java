92c35fb518e5451aecce1580da060f5ddf5f41d2
YEHUDA\yehuda <ylev1988@gmail.com>
2017-07-05 16:42:29 +0300

    final project

--- src/renderer/Render.java
+++ src/renderer/Render.java
@@ -22,8 +22,10 @@ public class Render {
 	private Scene _scene;
 	private ImageWriter _imageWriter;
 	private final int RECURSION_LEVEL = 3;
+	
+	//fields that we use for distribution of rays
 	Random random = new Random();
-	private int level = 20;
+	private int level = 15;
 
 	// full cons
 	public Render(ImageWriter imageWriter, Scene scene) {
@@ -36,6 +38,7 @@ public class Render {
 		this(render._imageWriter, render._scene);
 	}
 
+	//Constructor with one parameter "level" which is how many rays to distribute by pixel 
 	public Render(int level) {
 		_scene = new Scene();
 		this.level = level;
@@ -77,11 +80,14 @@ public class Render {
 
 	// services for the render-image operation
 	private Map<Geometry, List<Point3D>> getSceneRayIntersections(Ray ray) {
+		//allocate iterator that pass every geometric in the scene and finds every geometric's points intersections 
 		Iterator<Geometry> geometries = _scene.getGeometriesIterator();
+		//allocate map that contains each geometric (key) with his intersections points
 		Map<Geometry, List<Point3D>> intersectionPoints = new HashMap<Geometry, List<Point3D>>();
 		while (geometries.hasNext()) {
 			Geometry geometry = geometries.next();
 			List<Point3D> geometryIntersectionPoints = geometry.FindIntersections(ray);
+			//only if we find point(s) intersection we add to the container, others, we ignore. 
 			if (geometryIntersectionPoints.size() > 0)
 				intersectionPoints.put(geometry, geometryIntersectionPoints);
 		}
@@ -90,35 +96,42 @@ public class Render {
 
 	// render image
 	public void renderImage() {
-		// list of colors of specific pixel
+		//allocate an array list that holds for each pixel his colors, that we calculate from distribution rays 
 		ArrayList<Color> pixelColorsList = new ArrayList<>();
-
 		Map<Geometry, List<Point3D>> intersectionPoints = new HashMap<Geometry, List<Point3D>>();
 
+		//fields that we use for calculate range of random of half with and half height
+		//width and height of an specific pixel  
 		double w_per_pix = _imageWriter.getWidth() / _imageWriter.getNx();
 		double h_per_pix = _imageWriter.getHeight() / _imageWriter.getNy();
 		for (int i = 0; i < _imageWriter.getNx(); i++) {
-			for (int j = 0; j < _imageWriter.getNy(); j++) {
+			for (int j = 0; j < _imageWriter.getNy(); j++) { //here the internal loop we shot distribution ray per pixel (by given level)
 				for (int k = 0; k < level; k++) {
-					double r1 = random.nextDouble() - 0.5; // random factor
+					double r1 = random.nextDouble() - 0.5; // random range
 															// [-0.5, 0.5]
-					double r2 = random.nextDouble() - 0.5; // random factor
+					double r2 = random.nextDouble() - 0.5; // random range
 															// [-0.5, 0.5]
+					//here we calculate the final range that we allowed, [-0.5*width, +0.5*width] and [-0.5*width, +0.5*width]. 
 					double ex_i = r1 * w_per_pix + i;
 					double ex_j = r2 * h_per_pix + j;
 
+					//shot ray
 					Ray ray = _scene.getCamera().constructRayThroughPixel(_imageWriter.getNx(), _imageWriter.getNy(),
 							ex_i, ex_j, _scene.getScreenDistance(), _imageWriter.getWidth(), _imageWriter.getHeight());
 
+					//finds intersections points for the ray above
 					intersectionPoints = getSceneRayIntersections(ray);
 					if (intersectionPoints.isEmpty()) {
-						pixelColorsList.add(_scene.getBackground());
+						pixelColorsList.add(_scene.getBackground());  //no points, then add background color
 					}
 					else {
+						//found points, then we calculate the closet points and add it to the array list for average
 						Map<Geometry, Point3D> closestPoint = getClosestPoint(intersectionPoints);
 						pixelColorsList.add(calcColor((Geometry) (closestPoint.keySet().toArray()[0]), (Point3D) closestPoint.get(closestPoint.keySet().toArray()[0]), ray));
 					}
-				}
+				} //here end of 3rd of internal loop
+				
+				//calculate the average color by given array list of colors 
 				int _red = 0;
                 int _green = 0;
                 int _blue = 0;
@@ -140,11 +153,14 @@ public class Render {
 
 	// get the closet point to calculate the color for our image
 	private Map<Geometry, Point3D> getClosestPoint(Map<Geometry, List<Point3D>> intersectionPoints) {
+		//initial the distance id the max that double can hold
 		double distance = Double.MAX_VALUE;
+		//the closet point is the most close to the P0
 		Point3D P0 = _scene.getCamera().getP0();
 		Map<Geometry, Point3D> minDistancePoint = new HashMap<Geometry, Point3D>();
 		for (Entry<Geometry, List<Point3D>> entry : intersectionPoints.entrySet()) {
 			for (Point3D point : entry.getValue()) {
+				//if we find closer point than the last closet point that calculated then we initial it
 				if (P0.distance(point) < distance) {
 					minDistancePoint.clear();
 					minDistancePoint.put(entry.getKey(), new Point3D(point));
@@ -155,57 +171,65 @@ public class Render {
 		return minDistancePoint;
 	}
 
-	// calculate point color
+	// calculate point color- 3 parameters, the user doesn't need to figure out the 4th parameters 
 	private Color calcColor(Geometry geometry, Point3D point, Ray ray) {
 		return calcColor(geometry, point, ray, 0);
 	}
 
-	// calculate point color
+	// calculate point color- 4 parameters 
 	private Color calcColor(Geometry geometry, Point3D point, Ray inRay, int level) {
-
+		//the base condition stop the recursive calls
 		if (level == RECURSION_LEVEL) {
 			return new Color(0, 0, 0);
 		}
-
+		//fields that will holds the parts of Phong model
 		Color emissionLight = geometry.get_emmission();
 		Color ambientLight = _scene.getAmbientLight().getIntensity();
 		Color diffuseLight = new Color(0, 0, 0);
 		Color specularLight = new Color(0, 0, 0);
+		//allocate iterator that will ask every source light what color he add to the specific point
 		Iterator<LightSource> lights = _scene.getLightsIterator();
-
 		while (lights.hasNext()) {
 			LightSource light = lights.next();
-
+			//we need to check if the point is not blocked by the source light, if then ignore it 
+			//(means: there is no what to calculate the diffuse and secular)
 			if (!occluded(light, point, geometry)) {
-
+				
+				//add diffuse to our scene
 				diffuseLight = addColors(diffuseLight, calcDiffusiveComp(geometry.get_material().getKd(),
 						geometry.getNormal(point), light.getL(point), light.getIntensity(point)));
+				//add specular to our scene 
 				specularLight = addColors(specularLight,
 						calcSpecularComp(geometry.get_material().getKs(), new Vector(point, _scene.getCamera().getP0()),
 								geometry.getNormal(point), light.getL(point), geometry.get_nShininess(),
 								light.getIntensity(point)));
 			}
 		}
-
+		//here we calculate the reflected and the refracted of the given geometric 
 		Vector vector = geometry.getNormal(point);
 		Ray reflectedRay = constructReflectedRay(vector, point, inRay);
 		Map<Geometry, Point3D> reflectedEntry = findClosesntIntersection(reflectedRay);
+		//we start from the 0 color and then we consume 
 		Color reflectedColor = new Color(0, 0, 0);
 		Color reflectedLight = new Color(0, 0, 0);
-
+		//if we got intersection then get it and calculate
 		if (!reflectedEntry.isEmpty()) {
 
+			//here we call recursive 
 			reflectedColor = calcColor(reflectedEntry.entrySet().iterator().next().getKey(),
 					reflectedEntry.entrySet().iterator().next().getValue(), reflectedRay, level + 1);
+			//when we come back from our recursive then we multiplex the the material (which is the parameter of the reflected in this point)
 			reflectedLight = multColor(reflectedColor, geometry.get_material().getKr());
 		}
 
+		//calculate the refracted 
 		Ray refractedRay = constructRefractedRay(geometry, point, inRay);
 		Map<Geometry, Point3D> refractedEntry = findClosesntIntersection(refractedRay);
 		Color refractedColor = new Color(0, 0, 0);
 		Color refractedLight = new Color(0, 0, 0);
 
 		if (!refractedEntry.isEmpty()) {
+			//recursive call
 			refractedColor = calcColor(refractedEntry.entrySet().iterator().next().getKey(),
 					refractedEntry.entrySet().iterator().next().getValue(), refractedRay, level + 1);
 			refractedLight = multColor(refractedColor, geometry.get_material().getKt());
